d2b677d8fdf68d5878fd2cc1dec49d18b0711cb9
hamdaniIT <HamdaniIT@github.com>
2019-06-11 22:55:09 +0800

    Menambahkan file item cardview_president.xml, CustomOnItemClickListener, dan card ViewPresidentAdapter.java

---
+++ app/src/main/java/com/example/latihanivrecyclerview/CardViewPresidentAdapter.java
@@ -0,0 +1,26 @@
+package com.example.latihanivrecyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class CardViewPresidentAdapter extends RecyclerView.Adapter<CardViewPresidentAdapter.CardViewViewHolder>{
+    @NonNull
+    @Override
+    public CardViewPresidentAdapter.CardViewViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewPresidentAdapter.CardViewViewHolder cardViewViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class CardViewViewHolder {
+    }
+}
